d8f33500901ec675ec3825bea7f6b41e62014e17
Dinesh <dk6360159@gmail.com>
2026-07-26 20:17:09 +0530

    core files

---
+++ temp.dart
@@ -0,0 +1,16 @@
+final results= [
+{
+"gender": "male",
+"name": {},
+"location": {},
+"email": "elmer.richardson@example.com",
+"login": {},
+"dob": {},
+"registered": {},
+"phone": "(345) 263-1013",
+"cell": "(325) 386-5160",
+"id": {},
+"picture": {},
+"nat": "US"
+}
+];
